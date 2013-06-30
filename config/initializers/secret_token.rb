bae74972531bf970c9354cfecef380eee82d02fa
Paul Fastner <paul@furnituredealer.net>
2013-06-30 09:14:42 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = '5e75001e7cbf44b00c7c88f53e2e42dbff9a91db36759657d34cb4f96849c205c099352b2133c8ecbb3a3cea3f3c29a5d774708d092601711cd0bd7f3522de0d'
